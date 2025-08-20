8e67ab5bdb2e24c483641e778ce2de8c5c161bfe
renuka90827 <renukaanguswamy5@gmail.com>
2025-08-20 18:19:58 +0530

    product of new numbers

--- HELLO.C
+++ HELLO.C
@@ -1,3 +1,23 @@
 #include<iostream.h>
 #imclude<stdio.h>
-printf("HELLO!");+*//////////////////
+printf("HELLO!");
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    cout << "Size of char: " << sizeof(char) << " byte" << endl;
+    cout << "Size of int: " << sizeof(int) << " bytes" << endl;
+    cout << "Size of float: " << sizeof(float) << " bytes" << endl;
+    cout << "Size of double: " << sizeof(double) << " bytes" << endl;
+    return 0;
+}
+
+
+
+
+Your Output
+Size of char: 1 byte
+Size of int: 4 bytes
+Size of float: 4 bytes
+Size of double: 8 byte
